a1ac8afabebcf89b33d725478ab4a2a255d0eaf5
Ivan Garcia <vavimayor159@gmail.com>
2017-09-18 14:00:59 -0500

    Moving the login to the navigator

--- components/navComponent/navCtrl.js
+++ components/navComponent/navCtrl.js
@@ -9,11 +9,45 @@
             controllerAs: 'vm'
         });
 
-        navCtrl.$inject = ['$firebaseArray']
-        function navCtrl($firebaseArray) {
-        var vm = this;
+        navCtrl.$inject = ['$firebaseArray', '$firebaseAuth'];
+        function navCtrl($firebaseArray, $firebaseAuth) {
 
+            var vm = this;
+            var auth = $firebaseAuth();
+
+            vm.loginWithGoogle = loginWithGoogle;
+
+            initialize();
+
+            /**
+             * Bootstrap the component
+             * */
+            function initialize() {
               $('.parallax').parallax();
+            }
+
+            /**
+             * Make a login with the google strategy
+             * */
+            function loginWithGoogle() {
+                auth.$signInWithPopup("google").then(function (result) {
+
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    var token = result.credential.accessToken;
+
+                    // The signed-in user info.
+                    var user = result.user;
+
+                    // Loged in correctly
+                    alert('Inicio de sesión conseguido!');
+
+                })
+                .catch(function (error) {
+
+                    // Error in login
+                    console.error(error)
+                })
+            }
 
             
 
